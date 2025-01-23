8cd904001be94b15778ef2a757cb6f8266573da2
Vegard Matthey <VegardMatthey@protonmail.com>
2025-01-23 13:55:54 +0100

    format javascript and css

--- app.js
+++ app.js
@@ -1,9 +1,8 @@
-const express = require('express');
-const path = require('path');
-const { BigQuery } = require('@google-cloud/bigquery');
-const fetch = require('node-fetch');
-require('dotenv').config();
-
+const express = require("express");
+const path = require("path");
+const { BigQuery } = require("@google-cloud/bigquery");
+const fetch = require("node-fetch");
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
@@ -12,89 +11,90 @@ app.use(express.json());
 
 const keyPath = process.env.GOOGLE_APPLICATION_CREDENTIALS;
 
-
 // Sett opp BigQuery-klienten
 const bigquery = new BigQuery({
-  keyFilename: path.resolve(__dirname, process.env.GOOGLE_APPLICATION_CREDENTIALS),
+	keyFilename: path.resolve(
+		__dirname,
+		process.env.GOOGLE_APPLICATION_CREDENTIALS,
+	),
 });
 
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
 // Rute for å servere HTML-filen
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+app.get("/", (req, res) => {
+	res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
-app.get('/alleRuter',async (req,res)=>{
-  try {
-    // SQL-spørring til BigQuery
-    const query = 'SELECT lineRef, stopPointRef, stopPointName, aimedDepartureTime, departureTime, aimedArrivalTime, arrivalTime, dayOfTheWeek FROM  `ent-data-sharing-ext-prd.realtime_siri_et.realtime_siri_et_last_recorded` WHERE dataSource = "SKY" AND operatingDate = "2025-01-13" AND lineRef="SKY:Line:3" ORDER BY aimedDepartureTime, lineRef LIMIT 1000;'; // Endre spørringen etter dine behov
-    const options = {
-      query: query,
-      location: 'EU', // Juster region om nødvendig
-    };
+app.get("/alleRuter", async (req, res) => {
+	try {
+		// SQL-spørring til BigQuery
+		const query =
+			'SELECT lineRef, stopPointRef, stopPointName, aimedDepartureTime, departureTime, aimedArrivalTime, arrivalTime, dayOfTheWeek FROM  `ent-data-sharing-ext-prd.realtime_siri_et.realtime_siri_et_last_recorded` WHERE dataSource = "SKY" AND operatingDate = "2025-01-13" AND lineRef="SKY:Line:3" ORDER BY aimedDepartureTime, lineRef LIMIT 1000;'; // Endre spørringen etter dine behov
+		const options = {
+			query: query,
+			location: "EU", // Juster region om nødvendig
+		};
 
-    // Utfør spørringen
-    const [rows] = await bigquery.query(options);
+		// Utfør spørringen
+		const [rows] = await bigquery.query(options);
 
-    // Send data som JSON til klienten
-    res.json(rows);
-  } catch (error) {
-    console.error('Error querying BigQuery:', error);
-    res.status(500).send('Error querying BigQuery');
-  }
-})
+		// Send data som JSON til klienten
+		res.json(rows);
+	} catch (error) {
+		console.error("Error querying BigQuery:", error);
+		res.status(500).send("Error querying BigQuery");
+	}
+});
 
-app.get('/skjema', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'skjema.html'));
+app.get("/skjema", (req, res) => {
+	res.sendFile(path.join(__dirname, "public", "skjema.html"));
 });
 
 // Mottar forespørselen fra skjemaet
-app.post('/query', async (req, res) => {
-  res.send('Data mottatt!');
+app.post("/query", async (req, res) => {
+	res.send("Data mottatt!");
 });
- 
 
+async function hentData(query, options, req, res) {
+	try {
+		// SQL-spørring til BigQuery
+		//const query = 'SELECT lineRef, stopPointRef, stopPointName, aimedDepartureTime, departureTime, aimedArrivalTime, arrivalTime, dayOfTheWeek FROM  `ent-data-sharing-ext-prd.realtime_siri_et.realtime_siri_et_last_recorded` WHERE dataSource = "SKY" AND operatingDate = "2025-01-13" AND lineRef="SKY:Line:20" ORDER BY aimedDepartureTime, lineRef LIMIT 10;'; // Endre spørringen etter dine behov
 
-async function hentData(query,options, req, res) {
-  try {
-    // SQL-spørring til BigQuery
-    //const query = 'SELECT lineRef, stopPointRef, stopPointName, aimedDepartureTime, departureTime, aimedArrivalTime, arrivalTime, dayOfTheWeek FROM  `ent-data-sharing-ext-prd.realtime_siri_et.realtime_siri_et_last_recorded` WHERE dataSource = "SKY" AND operatingDate = "2025-01-13" AND lineRef="SKY:Line:20" ORDER BY aimedDepartureTime, lineRef LIMIT 10;'; // Endre spørringen etter dine behov
-   
+		// Utfør spørringen
+		const [rows] = await bigquery.query(options);
 
-    // Utfør spørringen
-    const [rows] = await bigquery.query(options);
-
-    // Send data som JSON til klienten
-    res.json(rows);
-  } catch (error) {
-    console.error('Error querying BigQuery:', error);
-    res.status(500).send('Error querying BigQuery');
-  }
+		// Send data som JSON til klienten
+		res.json(rows);
+	} catch (error) {
+		console.error("Error querying BigQuery:", error);
+		res.status(500).send("Error querying BigQuery");
+	}
 }
 
 // Ruten for å hente data som JSON
-app.get('/data', async (req, res) => {
-  try {
-    // SQL-spørring til BigQuery
-    const query = 'SELECT lineRef, stopPointRef, stopPointName, aimedDepartureTime, departureTime, aimedArrivalTime, arrivalTime, dayOfTheWeek FROM  `ent-data-sharing-ext-prd.realtime_siri_et.realtime_siri_et_last_recorded` WHERE dataSource = "SKY" AND operatingDate = "2025-01-13" AND lineRef="SKY:Line:20" ORDER BY aimedDepartureTime, lineRef LIMIT 10;'; // Endre spørringen etter dine behov
-    const options = {
-      query: query,
-      location: 'EU', // Juster region om nødvendig
-    };
+app.get("/data", async (req, res) => {
+	try {
+		// SQL-spørring til BigQuery
+		const query =
+			'SELECT lineRef, stopPointRef, stopPointName, aimedDepartureTime, departureTime, aimedArrivalTime, arrivalTime, dayOfTheWeek FROM  `ent-data-sharing-ext-prd.realtime_siri_et.realtime_siri_et_last_recorded` WHERE dataSource = "SKY" AND operatingDate = "2025-01-13" AND lineRef="SKY:Line:20" ORDER BY aimedDepartureTime, lineRef LIMIT 10;'; // Endre spørringen etter dine behov
+		const options = {
+			query: query,
+			location: "EU", // Juster region om nødvendig
+		};
 
-    // Utfør spørringen
-    const [rows] = await bigquery.query(options);
+		// Utfør spørringen
+		const [rows] = await bigquery.query(options);
 
-    // Send data som JSON til klienten
-    res.json(rows);
-  } catch (error) {
-    console.error('Error querying BigQuery:', error);
-    res.status(500).send('Error querying BigQuery');
-  }
+		// Send data som JSON til klienten
+		res.json(rows);
+	} catch (error) {
+		console.error("Error querying BigQuery:", error);
+		res.status(500).send("Error querying BigQuery");
+	}
 });
 
 // Start serveren
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+	console.log(`Server running at http://localhost:${port}`);
 });
--- public/funksjoner.js
+++ public/funksjoner.js
@@ -1,34 +1,32 @@
-function gjennomsnittligForsinkelse(datajson){
-    let tidligeAnkomster=[]
-    let utenNegative=[]
-    let innenToMinutter
-    for (let row in data){
-        const rutetid=new Date(row.aimedArrivalTime)
-        const faktiskAnkomst=new Date(row.arrivalTime)
-        const deltaMillisekund=faktiskAnkomst-rutetid
-        const minutterForsinket=deltaMillisekund/60000
-        
-        if (minutterForsinket<0){
-            tidligeAnkomster.push(minutterForsinket)
-            utenNegative.push(0)
-        }else if(minutterForsinket<2){
-            innenToMinutter++
-            utenNegative.push(minutterForsinket)
-        }
-        else{
-            utenNegative.push(minutterForsinket)
-        }
-       
-        //regn ut forsinkelse
-        //hvis negativ forsinkelse, håndter (tidlig ankosmt)
-        //legg til i liste
+function gjennomsnittligForsinkelse(datajson) {
+	let tidligeAnkomster = [];
+	let utenNegative = [];
+	let innenToMinutter;
+	for (let row in data) {
+		const rutetid = new Date(row.aimedArrivalTime);
+		const faktiskAnkomst = new Date(row.arrivalTime);
+		const deltaMillisekund = faktiskAnkomst - rutetid;
+		const minutterForsinket = deltaMillisekund / 60000;
 
-    }
-    const snittForsinket= mean(utenNegative)
-    const tidligProsent=(tidligeAnkomster.length()/utenNegative.length)*100
-    const tidligsteAnkomst=min(tidligeAnkomster)
-    const senesteAnkomst=max(utenNegative)
-    return(snittForsinket,tidligProsent,tidligsteAnkomst,senesteAnkomst)
-    //regn ut gjennomsnittlig forsinkelse, samt andel avganger som kommer for tidlig. 
-    //returner gjennomsnittlig forsinkelse, prosentandel tidlige ankomster, tidligste ankomst (min(tidlige ankoster), seneste ankomst (max(forsinkelser)))
-}//prosentandel avganger som er forsinket?? og gjennomsnitt for alle avgangene, men for tidlig teller ikke positivt. 
+		if (minutterForsinket < 0) {
+			tidligeAnkomster.push(minutterForsinket);
+			utenNegative.push(0);
+		} else if (minutterForsinket < 2) {
+			innenToMinutter++;
+			utenNegative.push(minutterForsinket);
+		} else {
+			utenNegative.push(minutterForsinket);
+		}
+
+		//regn ut forsinkelse
+		//hvis negativ forsinkelse, håndter (tidlig ankosmt)
+		//legg til i liste
+	}
+	const snittForsinket = mean(utenNegative);
+	const tidligProsent = (tidligeAnkomster.length() / utenNegative.length) * 100;
+	const tidligsteAnkomst = min(tidligeAnkomster);
+	const senesteAnkomst = max(utenNegative);
+	return snittForsinket, tidligProsent, tidligsteAnkomst, senesteAnkomst;
+	//regn ut gjennomsnittlig forsinkelse, samt andel avganger som kommer for tidlig.
+	//returner gjennomsnittlig forsinkelse, prosentandel tidlige ankomster, tidligste ankomst (min(tidlige ankoster), seneste ankomst (max(forsinkelser)))
+} //prosentandel avganger som er forsinket?? og gjennomsnitt for alle avgangene, men for tidlig teller ikke positivt.
